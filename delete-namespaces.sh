dfa10bcaf747e5e913af1e98e4bd6b426d17ecae
Giorgos <eftichiou@hotmail.com>
2026-03-15 13:12:08 +0200

    7 modify backend API message in values.yaml to reflect test version 7.

--- delete-namespaces.sh
+++ delete-namespaces.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-k delete ns george-app --ignore-not-found --timeout=120s
-k delete ns istio-system --ignore-not-found --timeout=120s
+k delete ns george-app
+k delete ns istio-system
